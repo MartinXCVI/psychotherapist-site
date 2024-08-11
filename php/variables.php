a4324f78f761d1cbf4ed420155e2f046b82c6812
MartinXCVI <mdzg96@gmail.com>
2024-08-10 22:42:50 -0300

    added php variables file

---
+++ php/variables.php
@@ -0,0 +1,20 @@
+<?php
+
+$indexIntro = "Welcome";
+
+$myName = "Clara Winslow";
+$myProf = "Psychotherapist";
+$fullProf = "Psychotherapist & Life Coach";
+
+$lorem = [
+  "lorem5" => "Lorem ipsum dolor sit amet consectetur.",
+  "lorem10" => "Optio, labore accusamus! Dolorum ipsum nisi debitis, itaque magni soluta.",
+  "lorem15" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, repellendus? Incidunt quod vitae deserunt consequuntur.",
+  "lorem20" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo labore itaque cupiditate repellat pariatur maxime iure corrupti! Officia, at vel.",
+  "lorem25" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe officia veritatis animi labore quibusdam. Cupiditate porro minima id culpa vel dignissimos, ex sequi commodi illum?",
+  "lorem30" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe officia veritatis animi labore quibusdam. Cupiditate porro minima id culpa vel dignissimos, ex sequi commodi illum? Lorem ipsum dolor sit amet"
+];
+
+$siteTitle = "Psychotherapist Site";
+
+$testimonials = ['Sophia Gallac'];
