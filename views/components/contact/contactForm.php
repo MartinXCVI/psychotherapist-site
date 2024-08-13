1e14a928557c8b93d4e7b9b714fa5c29b1242bb8
MartinXCVI <mdzg96@gmail.com>
2024-08-12 23:59:21 -0300

    implemented web3forms functionality

--- views/components/contact/contactForm.php
+++ views/components/contact/contactForm.php
@@ -1,32 +1,37 @@
 <div class="container-fluid d-flex flex-column justify-content-center align-items-center py-5" id="form-wrapper">
   <h2 class="col-11 col-lg-7 col-xl-8 text-center mb-5 lh-base">Get Your Free Consultation By Completing And Submitting This Form</h2>
-  <form class="row g-3 col-12 col-lg-11">
+  <form class="row g-3 col-12 col-lg-11" action="https://api.web3forms.com/submit" method="POST">
+    <input type="hidden" name="access_key" value="205881c5-0bfa-4e93-9523-81f375f5c37d">
+    <input type="hidden" name="redirect" value="/">
+    <input type="hidden" name="subject" value="Contact Email">
+    <input type="hidden" name="from_name" value="Psychotherapist PHP Site">
+    <input type="checkbox" name="botcheck" id="" style="display: none;">
     <div class="col-md-6">
-      <label for="inputName" class="form-label">Name</label>
-      <input type="text" class="form-control" id="inputName" placeholder="Your name here">
+      <label for="inputName" class="form-label">Full Name</label>
+      <input type="text" name="fullname"  class="form-control" id="inputName" placeholder="Your name here" required>
     </div>
     <div class="col-md-6">
       <label for="inputEmail4" class="form-label">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Your email here">
+      <input type="email" name="email" class="form-control" id="inputEmail4" placeholder="Your email here" required>
     </div>
     <div class="col-md-6">
       <label for="inputPhone" class="form-label">Phone</label>
-      <input type="text" class="form-control" id="inputPhone" placeholder="Your phone here">
+      <input type="text" name="phone" class="form-control" id="inputPhone" placeholder="Your phone here" required>
     </div>
     <div class="col-md-6">
       <label for="inputAddress" class="form-label">Address</label>
-      <input type="text" class="form-control" id="inputAddress" placeholder="Your address here">
+      <input type="text" name="address" class="form-control" id="inputAddress" placeholder="Your address here" required>
     </div>
     <div class="col-md-6">
       <label for="inputCity" class="form-label">City</label>
-      <input type="text" class="form-control" id="inputCity" placeholder="Your city here">
+      <input type="text" name="city" class="form-control" id="inputCity" placeholder="Your city here" required>
     </div>
     <div class="col-md-6">
       <label for="inputDate" class="form-label">Date of Birth</label>
-      <input type="date" class="form-control" id="inputDate">
+      <input type="date" name="birthdate" class="form-control" id="inputDate" required>
     </div>
     <div class="form-floating">
-      <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 150px"></textarea>
+      <textarea class="form-control" name="message" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 150px" required></textarea>
       <label for="floatingTextarea2" id="textarea-label">Message</label>
     </div>
     <div class="col-12 d-flex flex-column flex-sm-row justify-content-center mt-4">
